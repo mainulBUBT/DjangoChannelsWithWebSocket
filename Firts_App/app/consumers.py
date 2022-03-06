3828f4be499509d61b941b49479498429348928a
mainulBUBT <shopniilmihan@gmail.com>
2022-03-06 16:05:46 +0600

    first app of websocket success

---
+++ Firts_App/app/consumers.py
@@ -0,0 +1,26 @@
+from channels.consumer import SyncConsumer, AsyncConsumer
+
+class MySyncConsumer(SyncConsumer):
+
+    def websocket_connect(self, event):
+        print("Websocket Connected..", event)
+        self.send({
+            'type': 'websocket.accept'
+        })
+
+    def websocket_receive(self, event):
+        print("Message received",event)
+
+    def websocket_disconnect(self, event):
+        print("Websocket disconnected..", event)
+
+class MyAsyncConsumer(AsyncConsumer):
+
+    async def websocket_connect(self, event):
+        print("Websocket Connected..")
+
+    async def websocket_receive(self, event):
+        print("Message received")
+
+    async def websocket_disconnect(self, event):
+        print("Websocket disconnected..")
